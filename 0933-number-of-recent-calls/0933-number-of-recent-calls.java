f0e6472aceac898e7ab532a572f713c8d8655573
pavankumar <vtu28925@veltech.edu.in>
2026-09-16 10:25:48 +0530

    Time: 22 ms (49.67%), Space: 59.5 MB (48.21%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class RecentCounter {
+
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        // Remove requests older than t - 3000
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
